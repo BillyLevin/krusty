2c048abb5be6f8c11bd64e6e9982199d2bff5ff7
Billy Levin <billy.levin97@gmail.com>
2024-05-23 21:55:09 +0100

    refactor and clear current square from blocker mask

--- src/generate_magics.rs
+++ src/generate_magics.rs
@@ -7,27 +7,27 @@ use crate::{
 };
 
 pub fn generate_rook_blocker_mask(square: Square) -> anyhow::Result<Bitboard> {
+    const DIRECTIONS: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+
     let mut blockers = EMPTY_BB;
 
-    let start_rank = square.index() / 8;
-    let start_file = square.index() % 8;
+    let start_rank = (square.index() / 8) as i32;
+    let start_file = (square.index() % 8) as i32;
 
-    for rank in (start_rank + 1)..=6 {
-        blockers |= Square::new(rank.try_into()?, start_file.try_into()?).bitboard();
+    for (rank_offset, file_offset) in DIRECTIONS {
+        let mut rank = start_rank;
+        let mut file = start_file;
+
+        while rank > 0 && rank <= 6 && file > 0 && file <= 6 {
+            blockers |=
+                Square::new((rank as usize).try_into()?, (file as usize).try_into()?).bitboard();
+
+            rank += rank_offset;
+            file += file_offset;
+        }
     }
 
-    for rank in (1..=(start_rank - 1)).rev() {
-        blockers |= Square::new(rank.try_into()?, start_file.try_into()?).bitboard();
-    }
-
-    for file in (start_file + 1)..=6 {
-        blockers |= Square::new(start_rank.try_into()?, file.try_into()?).bitboard();
-    }
-
-    for file in (1..=(start_file - 1)).rev() {
-        blockers |= Square::new(start_rank.try_into()?, file.try_into()?).bitboard();
-    }
-
+    blockers.clear_bit(square);
     Ok(blockers)
 }
 
@@ -52,5 +52,6 @@ pub fn generate_bishop_blocker_mask(square: Square) -> anyhow::Result<Bitboard> 
         }
     }
 
+    blockers.clear_bit(square);
     Ok(blockers)
 }
